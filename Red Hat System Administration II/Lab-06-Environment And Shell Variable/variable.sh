4b931d129154497889c7ba8db268cd10471ae35e
khankhelasim <khankhelasim@gmail.com>
2025-10-26 18:03:00 +0500

    Add greeting script with user and date
    Create a script to greet the user with the current date.

---
+++ Red Hat System Administration II/Lab-06-Environment And Shell Variable/variable.sh
@@ -0,0 +1,12 @@
+
+---
+
+#### **2️⃣ Script Files**
+Place these files in the same folder as `README.md`.
+
+**variables.sh**
+```bash
+#!/bin/bash
+greeting="Welcome"
+user=$(whoami)
+echo "$greeting $user! Today is $(date)"
